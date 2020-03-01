65794099a4ab70a9fb4a60ba271ff93b1177879e
java <java@192.168.0.4>
2020-03-01 11:50:13 +0530

    if progams

---
+++ CoreProgramming/src/controlstatments/IfDemo.java
@@ -0,0 +1,14 @@
+package controlstatments;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int i =6;
+		
+if(i<9) {//--> optional when if body contains only one stat
+	System.out.println("iValue is less than 9 ");
+}
+System.out.println("welcome");
+	}
+
+}
